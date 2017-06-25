1423869a0dc7bec1204af20196febefeb531be1a
Thomas Champagne <th.champagne@gmail.com>
2017-06-25 18:05:52 +0200

    Rename main component as webapp

--- plugin/webapp/app/app.component.ts
+++ plugin/webapp/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'my-app',
+    selector: 'webapp',
     template: `<h1>Hello {{name}}</h1>
     <button md-raised-button>OK</button>
     <button md-icon-button [mdMenuTriggerFor]="menu">
